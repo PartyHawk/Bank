ba74ca8d09c52b6f7aa011662dde8ae2c26c9eea
Pim van den Toorn <pimvdtoorn@hotmail.com>
2020-05-14 13:02:49 +0200

    iets

--- Bank.java
+++ Bank.java
@@ -1,1 +1,21 @@
 //https://git-scm.com/downloads
+
+// git config --global user.name ""
+// git config --global user.email ""
+
+// git init
+// git status
+// git log
+// git add .                . = alles
+// git commit -m ""
+
+// git checkout [hash] 
+// git checkout -b [name]   checkout to an new branch
+// git branch <name>        geen naam geeft lijst, -d voor delete
+
+
+// git remote -v            alleremotes
+// git remote add origin [link]
+// git push -u origin master    -u geeft aan vanaf nu altijd naar origin master
+
+// git pull origin master
